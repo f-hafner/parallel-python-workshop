673530b6df2d93fa947bf12fecaa6899b2cd9117
Flavio Hafner <f.hafner@esciencecenter.nl>
2024-03-18 21:40:36 +0100

    update pdfs

--- teaching/notes/make_pdfs.sh
+++ teaching/notes/make_pdfs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-jupyter nbconvert --execute --to html --output temp.html benchmarking.ipynb
-wkhtmltopdf temp.html benchmarking.pdf
+# jupyter nbconvert --execute --to html --output temp.html benchmarking.ipynb
+# wkhtmltopdf temp.html benchmarking.pdf
 
 jupyter nbconvert --execute --to html --output temp.html computing_pi.ipynb
 wkhtmltopdf temp.html computing_pi.pdf
+
+jupyter nbconvert --execute --to html --output temp.html threads_and_processes.ipynb
+wkhtmltopdf temp.html threads_and_processes.pdf
 
 rm -rf temp.html
