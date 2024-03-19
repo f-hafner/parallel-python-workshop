dae96170ee6addff06d793f2151fa414d5e6cd06
Flavio Hafner <f.hafner@esciencecenter.nl>
2024-03-19 10:41:21 +0100

    update calc_pi

--- teaching/notes/calc_pi.py
+++ teaching/notes/calc_pi.py
@@ -1,13 +1,18 @@
 import random
 
-def calc_pi(N):
-    M = 0
+def calc_pi(N, name=None):
+    "Compute pi using N random samples"
+    printing = name is not None
+    if printing:
+        print(f"{name}: starting")  
+    M = 0 
     for i in range(N):
-        # Simulate impact coordinates
+        # Simulate random coordinates
         x = random.uniform(-1, 1)
         y = random.uniform(-1, 1)
-
-        # True if impact happens inside the circle
-        if x**2 + y**2 < 1.0:
+        if x**2 + y**2 < 1: # don't need sqrt b/c 1**2 = 1
             M += 1
-    return (4 * M / N, N)  # result, iterations
+
+    if printing:
+        print(f"{name}: Done")
+    return 4*M/N
